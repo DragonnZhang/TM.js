51cb852d49a330be864233885e4ab92582b9e830
DragonnZhang <731557579@qq.com>
2023-07-05 20:15:19 +0800

    feat: provided example to run project

---
+++ src/example/test.ts
@@ -0,0 +1,54 @@
+import * as TM from '../index'
+import './index.css'
+
+const el: HTMLCanvasElement = document.querySelector('#c') as HTMLCanvasElement
+const manual = TM.init(el)
+manual.setOption({
+  models: [
+    {
+      id: '1',
+      file: './cube.obj'
+    },
+    {
+      id: '2',
+      file: './bugatti.obj'
+    }
+  ],
+  steps: [
+    {
+      name: 'step 1',
+      objs: [
+        {
+          id: '1'
+        }
+      ]
+    },
+    {
+      name: 'step 2',
+      objs: [
+        {
+          id: '2'
+        }
+      ]
+    }
+  ]
+})
+
+const ele = document.querySelector('.button')!
+const button = document.createElement('button')
+button.onclick = triggerNext
+button.innerText = 'next'
+ele.appendChild(button)
+
+function triggerNext() {
+  manual.showNext()
+}
+
+const button2 = document.createElement('button')
+button2.onclick = triggerPrev
+button2.innerText = 'prev'
+ele.appendChild(button2)
+
+function triggerPrev() {
+  manual.showPrev()
+}
