0fdc974ea036c4f8a2a3b313cd9e05eb1113d333
DragonnZhang <731557579@qq.com>
2023-07-09 15:01:22 +0800

    fix: simplyfy the example

--- src/example/test.ts
+++ src/example/test.ts
@@ -39,28 +39,20 @@ const { showPrev, showNext, jumpToStep } = manual
 
 const ele = document.querySelector('.button')!
 const button = document.createElement('button')
-button.onclick = triggerNext
+button.onclick = showPrev
 button.innerText = 'next'
 ele.appendChild(button)
 
-function triggerNext() {
-  showNext()
-}
-
 const button2 = document.createElement('button')
-button2.onclick = triggerPrev
+button2.onclick = showNext
 button2.innerText = 'prev'
 ele.appendChild(button2)
 
-function triggerPrev() {
-  showPrev()
-}
-
 document.onkeyup = (e) => {
   if (e.key === 'ArrowLeft') {
-    triggerPrev()
+    showPrev()
   }
   else if (e.key === 'ArrowRight') {
-    triggerNext()
+    showNext()
   }
 }
