81bde0ac4235fdff7bed551744cbcdf5d35460c0
DragonnZhang <731557579@qq.com>
2023-07-15 22:58:31 +0800

    added ldraw loader

--- src/loader/loader.ts
+++ src/loader/loader.ts
@@ -1,12 +1,14 @@
 import { Group } from 'three'
 import { LoadingManager } from 'three'
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
+import { LDrawLoader } from 'three/examples/jsm/loaders/LDrawLoader.js'
 
 class ModelLoader {
   private manager = new LoadingManager()
 
   constructor() {
     this.manager.addHandler(/\.obj$/i, new OBJLoader())
+    this.manager.addHandler(/\.mpd$/i, new LDrawLoader())
   }
 
   async loadAsync(file: string): Promise<Group> {
